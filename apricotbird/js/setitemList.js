a716ff34f31925b5c7d9e429a4537a6a466905c5
sk <sk.ddb.rnk@gmail.com>
2017-09-22 23:22:02 +0900

    ソート機能追加

--- apricotbird/js/setitemList.js
+++ apricotbird/js/setitemList.js
@@ -8,46 +8,91 @@ $(document).ready(function(){
 
 itemListName = new Array(
 '',
-'icn/icn_itemTypePyw',/*001*/
-'icn/icn_itemTypePpk',/*002*/
-'icn/icn_itemTypePpk',/*003*/
-'icn/icn_itemTypeBpu',/*004*/
-'icn/icn_itemTypeBge',/*005*/
-'icn/icn_itemTypeBrd',/*006*/
-'icn/icn_itemTypeOgy',/*007*/
-'icn/icn_itemTypeOgy',/*008*/
-'icn/icn_itemTypeOgy',/*009*/
-'icn/icn_itemTypeRsv',/*010*/
-'icn/icn_itemTypePrd',/*011*/
-'icn/icn_itemTypeNsv',/*012*/
-'icn/icn_itemTypeNbu',/*013*/
-'icn/icn_itemTypeNbl',/*014*/
-'icn/icn_itemTypeNbu',/*015*/
-'icn/icn_itemTypeBbr',/*016*/
-'icn/icn_itemTypePgl',/*017*/
-'icn/icn_itemTypePgl',/*018*/
-'icn/icn_itemTypePsv',/*019*/
-'icn/icn_itemTypePsv',/*020*/
-'icn/icn_itemTypePpk',/*021*/
-'icn/icn_itemTypePge',/*022*/
-'icn/icn_itemTypePbu',/*023*/
-'icn/icn_itemTypePpk',/*024*/
-'icn/icn_itemTypePge',/*025*/
-'icn/icn_itemTypePbu',/*026*/
-'icn/icn_itemTypePpk',/*027*/
-'icn/icn_itemTypePge',/*028*/
-'icn/icn_itemTypePbu',/*029*/
-'icn/icn_itemTypePrd',/*030*/
-'icn/icn_itemTypePbu',/*031*/
-'icn/icn_itemTypePyw',/*032*/
-'icn/icn_itemTypePgy',/*033*/
-'icn/icn_itemTypeBpk',/*034*/
-'icn/icn_itemTypeBge',/*035*/
-'icn/icn_itemTypeByw',/*036*/
-'icn/icn_itemTypeBpk',/*037*/
-'icn/icn_itemTypeBbu',/*038*/
-'icn/icn_itemTypeBGe',/*039*/
-'icn/icn_itemTypePgy'/*040*/
+'icn_itemTypePyw',/*001*/
+'icn_itemTypePpk',/*002*/
+'icn_itemTypePpk',/*003*/
+'icn_itemTypeBpu',/*004*/
+'icn_itemTypeBge',/*005*/
+'icn_itemTypeBrd',/*006*/
+'icn_itemTypeOgy',/*007*/
+'icn_itemTypeOgy',/*008*/
+'icn_itemTypeOgy',/*009*/
+'icn_itemTypeRsv',/*010*/
+'icn_itemTypePrd',/*011*/
+'icn_itemTypeNsv',/*012*/
+'icn_itemTypeNbu',/*013*/
+'icn_itemTypeNbl',/*014*/
+'icn_itemTypeNbu',/*015*/
+'icn_itemTypeBbr',/*016*/
+'icn_itemTypePgl',/*017*/
+'icn_itemTypePgl',/*018*/
+'icn_itemTypePsv',/*019*/
+'icn_itemTypePsv',/*020*/
+'icn_itemTypePpk',/*021*/
+'icn_itemTypePge',/*022*/
+'icn_itemTypePbu',/*023*/
+'icn_itemTypePpk',/*024*/
+'icn_itemTypePge',/*025*/
+'icn_itemTypePbu',/*026*/
+'icn_itemTypePpk',/*027*/
+'icn_itemTypePge',/*028*/
+'icn_itemTypePbu',/*029*/
+'icn_itemTypePrd',/*030*/
+'icn_itemTypePbu',/*031*/
+'icn_itemTypePyw',/*032*/
+'icn_itemTypePgy',/*033*/
+'icn_itemTypeBpk',/*034*/
+'icn_itemTypeBge',/*035*/
+'icn_itemTypeByw',/*036*/
+'icn_itemTypeBpk',/*037*/
+'icn_itemTypeBbu',/*038*/
+'icn_itemTypeBGe',/*039*/
+'icn_itemTypePgy'/*040*/
+);
+mnum = 1 * 41;
+
+itemListType = new Array(
+'',
+'ear',/*001*/
+'ear',/*002*/
+'ear',/*003*/
+'arm',/*004*/
+'arm',/*005*/
+'arm',/*006*/
+'ear',/*007*/
+'ear',/*008*/
+'ear',/*009*/
+'arm',/*010*/
+'ear',/*011*/
+'neck',/*012*/
+'neck',/*013*/
+'neck',/*014*/
+'neck',/*015*/
+'arm',/*016*/
+'ear',/*017*/
+'ear',/*018*/
+'ear',/*019*/
+'ear',/*020*/
+'ear',/*021*/
+'ear',/*022*/
+'ear',/*023*/
+'ear',/*024*/
+'ear',/*025*/
+'ear',/*026*/
+'ear',/*027*/
+'ear',/*028*/
+'ear',/*029*/
+'ear',/*030*/
+'ear',/*031*/
+'ear',/*032*/
+'ear',/*033*/
+'arm',/*034*/
+'arm',/*035*/
+'arm',/*036*/
+'arm',/*037*/
+'arm',/*038*/
+'arm',/*039*/
+'ear'/*040*/
 );
 mnum = 1 * 41;
 
@@ -56,10 +101,10 @@ function itemList(){
         var noNum = i;
         var resetNum = ('000' + noNum).slice(-3);
         document.write('<a href="/html/item/item'+resetNum+'.html">');
-        document.write('<dl class="itemBox triMdl" id="wdi'+resetNum+'" data-tgt="wdi'+resetNum+'">');
+        document.write('<dl class="itemBox triMdl '+itemListType[i]+'" id="wdi'+resetNum+'" data-tgt="wdi'+resetNum+'">');
         document.write('<div class="itemMask">&nbsp;</div>');
         document.write('<dt><img src="/img/item/item'+resetNum+'_01.jpg" width="80" height="80"></dt>');
-        document.write('<dd class="itemIcon"><img src="/img/'+itemListName[i]+'.png" width="20"></dd>');
+        document.write('<dd class="itemIcon"><img src="/img/icn/'+itemListName[i]+'.png" width="20"></dd>');
         document.write('<dd class="itemNo">No.'+resetNum+'</dd>');
         document.write('</dl>');
         document.write('</a>');
